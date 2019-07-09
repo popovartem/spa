2bce22a527f163c20f072b7ee66642e729f0afd9
Popov Artem <popovartem@inbox.ru>
2019-07-09 18:09:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
- * app.js - Express server with basic auth
+ * app.js - Express server with routing
 */
 
 /*jslint         node    : true, continue : true,
@@ -13,42 +13,41 @@
 // ------------ BEGIN MODULE SCOPE VARIABLES --------------
 'use strict';
 var
-    http = require('http'),
-    express = require('express'),
-    routes = require('./routes'),
+  http    = require( 'http'     ),
+  express = require( 'express'  ),
+  routes  = require( './routes' ),
 
-    app = express(),
-    server = http.createServer(app);
+  app     = express(),
+  server  = http.createServer( app );
 // ------------- END MODULE SCOPE VARIABLES ---------------
 
 // ------------- BEGIN SERVER CONFIGURATION ---------------
-app.configure(function () {
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.basicAuth('user', 'spa'));
-    app.use(express.static(__dirname + '/public'));
-    app.use(app.router);
+app.configure( function () {
+  app.use( express.bodyParser() );
+  app.use( express.methodOverride() );
+  app.use( express.static( __dirname + '/public' ) );
+  app.use( app.router );
 });
 
-app.configure('development', function () {
-    app.use(express.logger());
-    app.use(express.errorHandler({
-        dumpExceptions: true,
-        showStack: true
-    }));
+app.configure( 'development', function () {
+  app.use( express.logger() );
+  app.use( express.errorHandler({
+    dumpExceptions : true,
+    showStack      : true
+  }) );
 });
 
-app.configure('production', function () {
-    app.use(express.errorHandler());
+app.configure( 'production', function () {
+  app.use( express.errorHandler() );
 });
 
-routes.configRoutes(app, server);
+routes.configRoutes( app, server );
 // -------------- END SERVER CONFIGURATION ----------------
 
 // ----------------- BEGIN START SERVER -------------------
-server.listen(3000);
+server.listen( 3000 );
 console.log(
-    'Express server listening on port %d in %s mode',
-    server.address().port, app.settings.env
+  'Express server listening on port %d in %s mode',
+   server.address().port, app.settings.env
 );
 // ------------------ END START SERVER --------------------
